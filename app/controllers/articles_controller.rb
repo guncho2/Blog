8f31045c544349e9fae2ecdbc4e7a18990780fd5
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-09-29 20:41:25 -0300

    created a new database, added show and create to the article controller saving the input in def create and rendering the data input to show html, creating an html shoeing data with the @article instance

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,17 +5,45 @@ class ArticlesController < ApplicationController
 
     end
 
-    def create
-
-        render plain: params.require(:article).inspect
-
-#rendering to the webpage/view article/new where article is the controller and the resources route
-#to know where submit the data on the form f from the web page article/new new plain means text
-    
-end
-
+    def  show
 
         
-end
+        @article = Article.find(params[:id])
+
+        # find an article with an specific ID  where the ID number is incremented automatically for each
+        #article 
+    end
+    
+    def create
+
+        # render plain: params.require(:article).inspect
+
+        @article = Article.new(article_params)
+
+        #creo un instanciade Article donde guardo el requerimento de permiso de title y text
+
+            #rendering to the webpage/view article/new where article is the controller and the resources route
+            #to know where submit the data on the form f from the web page article/new new plain means text
+
+        @article.save
+             #saving the input from view article
+
+                # now we need to redirect to appropiate page
+        redirect_to @article
+
+        #this will show the articles just created on the Show page 
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+
+        end
+        
+    end
+
+
+
+
 
 
