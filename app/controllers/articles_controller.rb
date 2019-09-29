c95f3b131e3e1bc0227f0c286b470e46ae2a4a5a
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-09-29 18:35:04 -0300

    added routes for form outputed to browser via inspect(on the article controller)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,21 @@
 class ArticlesController < ApplicationController
 
     def new
-        
+
+
     end
 
+    def create
+
+        render plain: params.require(:article).inspect
+
+#rendering to the webpage/view article/new where article is the controller and the resources route
+#to know where submit the data on the form f from the web page article/new new plain means text
+    
+end
+
+
+        
 end
 
 
